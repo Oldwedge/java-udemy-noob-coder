479d2e65f6bfcc80d9487e7deb30577a14cbad2e
Martin.Andersson <Martin.Andersson@CWL-MANDERSSON3.cambio.se>
2018-05-17 14:23:13 +0200

    done

--- src/Methods.java
+++ src/Methods.java
@@ -1,9 +1,26 @@
-
 public class Methods {
 
 	public static void main(String[] args) {
 		
-
+		int a = 3;
+		int b = 5;
+		
+		
+		report(a, b);
+		System.out.println("The sum of num1 and num2 is: " + add(a, b));
+		
+	}
+	
+	public static void report(int num1, int num2) {
+		
+		System.out.println("num1 has the value of " + num1 + ", num2 has the value of " + num2);
+			
+	}
+	
+	public static int add(int num1, int num2) {
+				
+		return num1 + num2;
+		
 	}
 
 }
