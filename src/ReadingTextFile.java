42e8a49fc7e3702b209a523da6a26628bf2bbd2e
Martin Andersson <k.martin.s.andersson@hotmail.com>
2018-05-21 09:18:12 +0200

    done

--- src/ReadingTextFile.java
+++ src/ReadingTextFile.java
@@ -1,7 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ReadingTextFile {
 
 	public static void main(String[] args) {
+		
+		File file = new File("C:/eclipse/workspace/Udemy-noobcoder/src/ReadTextFile.txt");
+		
+		Scanner input;
+		
+		try {
+			input = new Scanner(file);
+			
+			while (input.hasNextLine()) {
+				
+				System.out.println(input.nextLine());
+				
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+
+			System.out.println("Unable to locate file specified: " + file.getAbsolutePath());
+			
+			e.printStackTrace();
+		}
 		
 		
 
