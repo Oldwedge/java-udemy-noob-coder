de71ff558270dc7e28befdb81990e66c84642468
Martin Andersson <k.martin.s.andersson@hotmail.com>
2018-05-21 07:41:23 +0200

    updates

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,10 +1,22 @@
 
+import java.util.Scanner;
+
 public class UserInput {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Enter your name:");
+		
+		String name = input.nextLine();
+		
+		System.out.print("Hello " + name);
+		
+		input.close();
 		
 	}
 	
 }
+
+
