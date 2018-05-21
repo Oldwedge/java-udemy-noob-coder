faa69298a8fb9e75de343eeac4788ee2152671fc
Martin Andersson <k.martin.s.andersson@hotmail.com>
2018-05-21 07:31:02 +0200

    yeah

--- src/Recursion.java
+++ src/Recursion.java
@@ -3,6 +3,20 @@ public class Recursion {
 	
 	public static void main(String[] args) {
 		
+		int x = 4;
+		
+		System.out.println(factorial(x));
+		
+	}
+	
+	public static int factorial(int num1) {
+		
+		if(num1 <= 1) {
+			return 1;
+			
+		}
+		
+		return num1 * factorial(num1 -1) ;
 		
 	}
 
